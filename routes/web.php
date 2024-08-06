786d9b3360ec8507d72892af47f0cf6358ebd93b
xemphimhay <lanhp871@gmail.com>
2024-08-06 08:37:48 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Ophim\ThemeIq\Controllers\ThemeIqController;
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+
+Route::group([
+    'middleware' => array_merge(
+        (array) config('backpack.base.web_middleware', 'web'),
+    ),
+], function () {
+    Route::get('/', [ThemeIqController::class, 'index']);
+
+    Route::get('/ajax-list-ep', [ThemeIqController::class, 'getListEpisodeAjax'])
+        ->name('movies.ajax_list_ep');
+
+
+    Route::get('/playeropt', [ThemeIqController::class, 'getPlayerOptAjax'])
+        ->name('movies.ajax_playeropt');
+
+    Route::get(setting('site_routes_category', '/the-loai/{category}'), [ThemeIqController::class, 'getMovieOfCategory'])
+        ->where(['category' => '.+', 'id' => '[0-9]+'])
+        ->name('categories.movies.index');
+
+    Route::get(setting('site_routes_region', '/quoc-gia/{region}'), [ThemeIqController::class, 'getMovieOfRegion'])
+        ->where(['region' => '.+', 'id' => '[0-9]+'])
+        ->name('regions.movies.index');
+
+    Route::get(setting('site_routes_tag', '/tu-khoa/{tag}'), [ThemeIqController::class, 'getMovieOfTag'])
+        ->where(['tag' => '.+', 'id' => '[0-9]+'])
+        ->name('tags.movies.index');
+
+    Route::get(setting('site_routes_types', '/danh-sach/{type}'), [ThemeIqController::class, 'getMovieOfType'])
+        ->where(['type' => '.+', 'id' => '[0-9]+'])
+        ->name('types.movies.index');
+
+    Route::get(setting('site_routes_actors', '/dien-vien/{actor}'), [ThemeIqController::class, 'getMovieOfActor'])
+        ->where(['actor' => '.+', 'id' => '[0-9]+'])
+        ->name('actors.movies.index');
+
+    Route::get(setting('site_routes_directors', '/dao-dien/{director}'), [ThemeIqController::class, 'getMovieOfDirector'])
+        ->where(['director' => '.+', 'id' => '[0-9]+'])
+        ->name('directors.movies.index');
+
+    Route::get(setting('site_routes_episode', '/phim/{movie}/{episode}-{id}'), [ThemeIqController::class, 'getEpisode'])
+        ->where(['movie' => '.+', 'movie_id' => '[0-9]+', 'episode' => '.+', 'id' => '[0-9]+'])
+        ->name('episodes.show');
+
+    Route::post(sprintf('/%s/{movie}/{episode}/report', config('ophim.routes.movie', 'phim')), [ThemeIqController::class, 'reportEpisode'])->name('episodes.report');
+    Route::post(sprintf('/%s/{movie}/rate', config('ophim.routes.movie', 'phim')), [ThemeIqController::class, 'rateMovie'])->name('movie.rating');
+
+    Route::get(setting('site_routes_movie', '/phim/{movie}'), [ThemeIqController::class, 'getMovieOverview'])
+        ->where(['movie' => '.+', 'id' => '[0-9]+'])
+        ->name('movies.show');
+    Route::get('/search/{search}', [ThemeIqController::class, 'search'])->name('search');
+});
